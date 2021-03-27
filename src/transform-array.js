27d577992e1855d1dbc037a53ddd94a918a60593
Denis Kulikov <9933denis@gmail.com>
2021-03-27 18:50:57 +0300

    feat: add transform-array and vigenere-cipher

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,7 +2,46 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
 
- throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+if(Array.isArray(arr) != true) {throw new Error()};
 
+let newArr = arr.slice();
+let del = 'del';
+
+  newArr.forEach((element, index) => {
+
+    if (element == '--discard-next') {
+      if (index != newArr.length - 1) {
+        newArr[index] = del;
+        newArr[index + 1] = del;
+      } else {
+        newArr[index] = del;
+      }
+    } else if (element == '--discard-prev') {
+
+        if (index != 0) {
+          newArr[index] = del;
+          newArr[index - 1] = del;
+        } else {
+          newArr[index] = del;
+        }
+    } else if (element == '--double-next') {
+
+      if (index != newArr.length - 1) {
+        newArr[index] = newArr[index + 1]
+      } else {
+        newArr[index] = del
+      }
+    } else if (element == '--double-prev') {
+
+      if (index != 0) {
+        newArr[index] = newArr[index - 1]
+      } else {
+        newArr[index] = del;
+      }
+    }
+
+  });
+ 
+ return newArr.filter(element => {return element != del}) 
+
+};
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,90 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor (revers) {
+    this.revers = revers;
   }
+
+  encrypt (message, key) {
+    let mesLowreCase = message.toLowerCase();
+    let mesToArr = mesLowreCase.replace(/\s/g, '').split('');
+    let keyUp = key.padEnd(mesToArr.length, key).toLowerCase().split('');
+    let count = 0;
+    let keyUpSpace = []
+    let result = [];
+
+    mesLowreCase.split('').forEach (element => {
+      
+      if (/[a-zA-Z]/.test(element) == false) {
+        keyUpSpace.push(element)
+      } else {
+        keyUpSpace.push(keyUp[count])
+        count++ 
+      }
+    })
+
+    mesLowreCase.split('').forEach((element, index) => {
+
+      if(/[a-zA-Z]/.test(element) == false) {
+        result.push(element)
+      } else { 
+          if( ((element.charCodeAt() - 97) + (keyUpSpace[index].charCodeAt() - 97)) >= 26 ) {
+            result.push( String.fromCharCode( (element.charCodeAt() - 97) + (keyUpSpace[index].charCodeAt() - 97) + 97 - 26) )
+          } else {
+            result.push( String.fromCharCode( (element.charCodeAt() - 97) + (keyUpSpace[index].charCodeAt() - 97) + 97) )
+          }
+      }
+    });
+
+    if(this.revers == false) {
+      return result.reverse().join('').toUpperCase();
+    } else {
+      return result.join('').toUpperCase();
+    }
+    
+  }
+
+  decrypt (message, key) {
+    let mesLowreCase = message.toLowerCase();
+    let mesToArr = mesLowreCase.replace(/\s/g, '').split('');
+    let keyUp = key.padEnd(mesToArr.length, key).toLowerCase().split('');
+    let count = 0;
+    let keyUpSpace = []
+    let result = [];
+
+    mesLowreCase.split('').forEach (element => {
+      
+      if (/[a-zA-Z]/.test(element) == false) {
+        keyUpSpace.push(element)
+      } else {
+        keyUpSpace.push(keyUp[count])
+        count++ 
+      }
+    })
+
+    mesLowreCase.split('').forEach((element, index) => {
+
+      if(/[a-zA-Z]/.test(element) == false) {
+        result.push(element)
+      } else { 
+          if( ((element.charCodeAt() - 97) - (keyUpSpace[index].charCodeAt() - 97)) < 0 ) {
+            result.push( String.fromCharCode( (element.charCodeAt() - 97) - (keyUpSpace[index].charCodeAt() - 97) + 97 + 26) )
+          } else {
+            result.push( String.fromCharCode( (element.charCodeAt() - 97) - (keyUpSpace[index].charCodeAt() - 97) + 97) )
+          }
+      }
+    });
+
+    if(this.revers == false) {
+      return result.reverse().join('').toUpperCase();
+    } else {
+      return result.join('').toUpperCase();
+    }
+  }
+
 }
 
 module.exports = VigenereCipheringMachine;
+
+
